886e52637ef001dd1d9ecdd294c43aa429d5f63b
Rafiul789 <89851417+Rafiul789@users.noreply.github.com>
2022-02-28 14:56:35 +0600

    info added

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const showPlayerDetails = (players) => {
     for (const player of players) {
         const parent = document.getElementById('players-container')
         const div = document.createElement('div')
-        div.innerHTML = `  <div class="card  border p-5">
+        div.innerHTML = `  <div class="card w-25 border p-5">
     <div class="pro-pic w-50">
         <img class="w-25" src="${player.strThumb}">
     </div>
@@ -21,10 +21,21 @@ const showPlayerDetails = (players) => {
     <p></p>
     <div class="all-button">
         <button class="btn btn-danger">Delete</button>
-        <button class="btn btn-success">Details</button>
+        <button onclick="details(${player.idPlayer})"  class="btn btn-success">Details</button>
     </div>
 </div>`
         parent.appendChild(div)
     }
 
+}
+
+const details = (id) => {
+    const url = `https://www.thesportsdb.com/api/v1/json/2/lookupplayer.php?id=${id}`
+    fetch(url)
+        .then((response) => response.json())
+        .then(data => setDetails(data.players[0]))
+}
+const setDetails = (info) => {
+
+    document.getElementById('detail-container').innerHTML = `<div>  <img src=""> <h1>Name:${info.strPlayer}  </div>`
 }
